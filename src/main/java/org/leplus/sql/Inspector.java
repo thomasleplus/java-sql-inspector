25a5456bdda2d8dd6e8570ea8dadba9aba452e5d
Thomas Leplus <thomasleplus@users.noreply.github.com>
2016-11-25 17:22:06 -0800

    Moving to github

---
+++ src/main/java/org/leplus/sql/Inspector.java
@@ -0,0 +1,27 @@
+package org.leplus.sql;
+
+import java.util.List;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.leplus.antlr4.PLSQLLexer;
+import org.leplus.antlr4.PLSQLParser;
+
+public class Inspector {
+
+	public Inspector() {
+		super();
+	}
+
+	public List<Result> findConstants(String sqlStatement) {
+		final PLSQLLexer lexer = new PLSQLLexer(new ANTLRInputStream(sqlStatement));
+		final CommonTokenStream tokens = new CommonTokenStream(lexer);
+		final PLSQLParser parser = new PLSQLParser(tokens);
+		final ParseTreeWalker walker = new ParseTreeWalker();
+		final ConstantListener listener = new ConstantListener();
+		walker.walk(listener, parser.data_manipulation_language_statements());
+		return listener.getResults();
+	}
+
+}
