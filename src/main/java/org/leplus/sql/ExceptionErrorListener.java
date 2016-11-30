0fa08e0a139caede47b6ec9f9cc3ca16f73896f7
Thomas Leplus <thomasleplus@users.noreply.github.com>
2016-11-30 03:06:40 -0800

    Simpler error listener

--- src/main/java/org/leplus/sql/ExceptionErrorListener.java
+++ src/main/java/org/leplus/sql/ExceptionErrorListener.java
@@ -1,33 +1,13 @@
 package org.leplus.sql;
 
-import java.util.BitSet;
-
-import org.antlr.v4.runtime.ANTLRErrorListener;
-import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
-import org.antlr.v4.runtime.atn.ATNConfigSet;
-import org.antlr.v4.runtime.dfa.DFA;
 
-public class ExceptionErrorListener implements ANTLRErrorListener {
+public class ExceptionErrorListener extends BaseErrorListener {
 
 	public ExceptionErrorListener() {
 		super();
-	}
-
-	@Override
-	public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact,
-			BitSet ambigAlts, ATNConfigSet configs) {
-	}
-
-	@Override
-	public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex,
-			BitSet conflictingAlts, ATNConfigSet configs) {
-	}
-
-	@Override
-	public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction,
-			ATNConfigSet configs) {
 	}
 
 	@Override
