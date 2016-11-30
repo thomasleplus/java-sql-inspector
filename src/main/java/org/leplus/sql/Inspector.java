806adbb7e33129ca7534e2e59087c010099735c7
Thomas Leplus <thomasleplus@users.noreply.github.com>
2016-11-30 03:04:40 -0800

    Handle parsing errors via RecognitionException instead of print

--- src/main/java/org/leplus/sql/Inspector.java
+++ src/main/java/org/leplus/sql/Inspector.java
@@ -18,6 +18,8 @@ public class Inspector {
 		final PLSQLLexer lexer = new PLSQLLexer(new ANTLRInputStream(sqlStatement));
 		final CommonTokenStream tokens = new CommonTokenStream(lexer);
 		final PLSQLParser parser = new PLSQLParser(tokens);
+		parser.removeErrorListeners();
+		parser.addErrorListener(new ExceptionErrorListener());
 		final ParseTreeWalker walker = new ParseTreeWalker();
 		final ConstantListener listener = new ConstantListener();
 		walker.walk(listener, parser.data_manipulation_language_statements());
